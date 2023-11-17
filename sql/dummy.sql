3a51dc3c161bb01144ae93787a9122d71206f635
Lzyne <raina2zen90@gmail.com>
2023-11-17 20:39:41 +0900

    테이블 생성 및 더미데이터 입력

---
+++ sql/dummy.sql
@@ -0,0 +1,14 @@
+INSERT INTO mvcboard (name, title, content, pass)
+	VALUES ('김유신', '자료실 제목 1 입니다.', '내용', '1234');
+	
+INSERT INTO mvcboard (name, title, content, pass)
+	VALUES ('장보고', '자료실 제목 2 입니다.', '내용', '1234');
+	
+INSERT INTO mvcboard (name, title, content, pass)
+	VALUES ('이순신', '자료실 제목 3 입니다.', '내용', '1234');
+	
+INSERT INTO mvcboard (name, title, content, pass)
+	VALUES ('강감찬', '자료실 제목 4 입니다.', '내용', '1234');
+	
+INSERT INTO mvcboard (name, title, content, pass)
+	VALUES ('대조영', '자료실 제목 5 입니다.', '내용', '1234');
---
+++ sql/tables.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS mvcboard;
+CREATE TABLE mvcboard(
+	idx int NOT NULL auto_increment PRIMARY KEY comment "일련번호",
+	name varchar(50) NOT NULL comment "작성자 이름",
+	title varchar(200) NOT NULL comment "제목",
+	content varchar(2000) NOT NULL comment "내용",
+	postdate date NOT NULL DEFAULT sysdate() comment "작성일",
+	ofile varchar(200) comment "원본 파일명",
+	sfile varchar(30) comment "저장된 파일명",
+	downcount decimal NOT NULL DEFAULT 0 comment "다운로드 횟수",
+	pass varchar(50) NOT NULL comment "비밀번호",
+	visitcount decimal NOT NULL DEFAULT 0 comment "조회수"
+);
