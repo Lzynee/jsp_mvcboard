96d0828c4e0a3c5c3c755738f4609657ea9b4be1
Lzynee <raina2zen90@gmail.com>
2023-11-17 22:16:25 +0900

    MVCBoard의 DTO, DAO 파일 생성

---
+++ src/main/java/mvcboard/MVCBoardDTO.java
@@ -0,0 +1,19 @@
+package mvcboard;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MVCBoardDTO {
+    // 멤버 변수 선언
+    private String idx;
+    private String name;
+    private String title;
+    private String content;
+    private java.sql.Date postdate;
+    private String ofile;
+    private String sfile;
+    private int downcount;
+    private String pass;
+    private int visitcount;
+}
