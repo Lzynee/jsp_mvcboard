a0d2fc8860e40dc4c2ec5301f91277795dfd154c
Lzynee <raina2zen90@gmail.com>
2023-11-19 16:56:37 +0900

    파일 다운로드 서블릿 작성

--- src/main/java/mvcboard/DownloadController.java
+++ src/main/java/mvcboard/DownloadController.java
@@ -1,6 +1,38 @@
+/**
+ * 파일 다운로드 서블릿
+ * 다운로드 링크 클릭 시 전달하는 매개변수를 받아와서
+ * 파일을 다운로드한 후
+ * 다운로드 횟수를 증가시킨다.
+ * */
+
 package mvcboard;
 
-import javax.servlet.http.HttpServlet;
+import utils.FileUtil;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/mvcboard/download.do")
 public class DownloadController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        // 매개변수 받기
+        String ofile = req.getParameter("ofile");  // 원본 파일명
+        String sfile = req.getParameter("sfile");  // 저장된 파일명
+        String idx = req.getParameter("idx");  // 게시물 일련번호
+
+        // 파일 다운로드
+        FileUtil.download(req, resp, "/Uploads", sfile, ofile);
+
+        // 해당 게시물의 다운로드 수 1 증가
+        MVCBoardDAO dao = new MVCBoardDAO();
+        dao.downCountPlus(idx);
+        dao.close();
+    }
 }
